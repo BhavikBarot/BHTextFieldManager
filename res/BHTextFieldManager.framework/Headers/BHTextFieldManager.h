06ae9af405df8219f300d99a44a5a173017cb63e
Bhavik Barot <39043956+BhavikBarot@users.noreply.github.com>
2018-09-19 14:04:12 +0530

    Add files via upload

---
+++ res/BHTextFieldManager.framework/Headers/BHTextFieldManager.h
@@ -0,0 +1,20 @@
+//
+//  BHTextFieldManager.h
+//  BHTextFieldManager
+//
+//  Created by Bhavik's Mac on 3/8/18.
+//  Copyright © 2018 Bhavik Barot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BHTextFieldManager.
+FOUNDATION_EXPORT double BHTextFieldManagerVersionNumber;
+
+//! Project version string for BHTextFieldManager.
+FOUNDATION_EXPORT const unsigned char BHTextFieldManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BHTextFieldManager/PublicHeader.h>
+
+#import<BHTextFieldManager/BHTextFieldManager+Accesory.h>
+
